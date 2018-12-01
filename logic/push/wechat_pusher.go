5f8a041e90809a6f5f81251af43e5acb3cf75f46
16 <1084910400@qq.com>
2018-12-01 11:07:29 +0800

    完成了微信推送的部分功能

--- logic/push/wechat_pusher.go
+++ logic/push/wechat_pusher.go
@@ -14,81 +14,97 @@ type WeChatPusher struct {
 }
 
 //获取公众号配置信息
-func getMpInfo() (*wechat.MpInfo,error){
-	
+func getMpInfo(config *map[string]string) (*wechat.MpInfo, error) {
+	mpinfo := wechat.MpInfo{}
+
+	app_id, ok := (*config)["app_id"]
+
+	if !ok || app_id == "" {
+		return nil, errors.New("wechat:缺乏参数 appid")
+	}
+	mpinfo.AppID = app_id
+
+	app_secret, ok := (*config)["app_secret"]
+	if !ok || app_secret == "" {
+		return nil, errors.New("wechat:缺乏参数 app_secret")
+	}
+	mpinfo.AppSecret = app_secret
+
+	token, ok := (*config)["token"]
+	if ok {
+		mpinfo.Token = token
+	} else {
+		mpinfo.Token = ""
+	}
+
+	encoding_aeskey, ok := (*config)["encoding_aeskey"]
+	if ok {
+		mpinfo.EncodingAESKey = encoding_aeskey
+	} else {
+		mpinfo.EncodingAESKey = ""
+	}
+
+	return &mpinfo, nil
+}
+
+func getTemplate(data *Info) (*template.Message, error) {
+	msg := template.Message{}
+
+	//获取模板消息
+	if data.User == "" {
+		return nil, errors.New("wechat:找不到推送用户")
+	}
+	msg.ToUser = data.User
+
+	template_id, ok := data.Message["template_id"]
+	if !ok || template_id == "" {
+		return nil, errors.New("wechat:缺乏 模板id")
+	}
+	msg.TemplateID = template_id.(string)
+
+	url, ok := data.Message["url"]
+	if ok {
+		msg.URL = url.(string)
+	} else {
+		msg.URL = ""
+	}
+
+	color, ok := data.Message["color"]
+	if ok {
+		msg.Color = color.(string)
+	} else {
+		msg.Color = ""
+	}
+
+	dataItems, ok := data.Message["data"]
+	if ok && reflect.TypeOf(dataItems).String() != "map[string]interface {}" {
+		return nil, errors.New("wechat:推送的数据格式错误")
+	}
+
+	msg.Data = make(map[string]*template.DataItem)
+	for key, dataItem := range dataItems.(map[string]interface{}) {
+		item := &template.DataItem{
+			Value: dataItem.(map[string]interface{})["value"].(string),
+			Color: dataItem.(map[string]interface{})["color"].(string),
+		}
+		msg.Data[key] = item
+	}
+
+	return &msg, nil
 }
 
 //验证推送数据是否符合要求
 func (p *WeChatPusher) Validate(data *Info) error {
 	//获取公众号信息
 	var err error
-	p.msg,err := getMpInfo(&data.Config)
-	
-	app_id, ok := data.Config["app_id"]
-
-	if !ok || app_id == "" {
-		return errors.New("wechat:缺乏参数 appid")
-	}
-	p.mpinfo.AppID = app_id
-
-	app_secret, ok := data.Config["app_secret"]
-	if !ok || app_secret == "" {
-		return errors.New("wechat:缺乏参数 app_secret")
-	}
-	p.mpinfo.AppSecret = app_secret
-
-	token, ok := data.Config["token"]
-	if ok {
-		p.mpinfo.Token = token
-	} else {
-		p.mpinfo.Token = ""
+	p.mpinfo, err = getMpInfo(&data.Config)
+	if err != nil {
+		return err
 	}
 
-	encoding_aeskey, ok := data.Config["encoding_aeskey"]
-	if ok {
-		p.mpinfo.EncodingAESKey = encoding_aeskey
-	} else {
-		p.mpinfo.EncodingAESKey = ""
-	}
-
-	//获取模板消息
-	if data.User == "" {
-		return errors.New("wechat:找不到推送用户")
-	}
-	p.msg.ToUser = data.User
-
-	template_id, ok := data.Message["template_id"]
-	if !ok || template_id == "" {
-		return errors.New("wechat:缺乏 模板id")
-	}
-	p.msg.TemplateID = template_id.(string)
-
-	url, ok := data.Message["url"]
-	if ok {
-		p.msg.URL = url.(string)
-	} else {
-		p.msg.URL = ""
-	}
-
-	color, ok := data.Message["color"]
-	if ok {
-		p.msg.Color = color.(string)
-	} else {
-		p.msg.Color = ""
-	}
-
-	dataItems, ok := data.Message["data"]
-	if ok && reflect.TypeOf(dataItems).String() != "map[string]interface {}" {
-		return errors.New("wechat:推送的数据格式错误")
-	}
-
-	p.msg.Data = make(map[string]*template.DataItem)
-	for key, dataItem := range dataItems.(map[string]interface{}) {
-		item := &template.DataItem{
-			Value: dataItem.(map[string]interface{})["value"].(string),
-			Color: dataItem.(map[string]interface{})["color"].(string),
-		}
-		p.msg.Data[key] = item
+	p.msg, err = getTemplate(data)
+	if err != nil {
+		return err
 	}
 
 	return nil
